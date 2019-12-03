8dd36fd0534c0b6dce8b6e939002b227c2ea342c
klipinski <karol.lipinski@fenige.pl>
2019-12-03 14:32:47 +0100

    Init soap

---
+++ src/main/java/net/atos/springsoapserver/SpringSoapServerApplication.java
@@ -0,0 +1,13 @@
+package net.atos.springsoapserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapServerApplication.class, args);
+	}
+
+}
