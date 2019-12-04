846a22b984445450922d4a9580710a7d71353aaf
klipinski <karol.lipinski@fenige.pl>
2019-12-04 08:36:57 +0100

    Update

--- src/main/java/net/atos/springsoapserver/config/WebServiceConfig.java
+++ src/main/java/net/atos/springsoapserver/config/WebServiceConfig.java
@@ -1,10 +1,15 @@
 package net.atos.springsoapserver.config;
 
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
 @EnableWs
@@ -22,6 +27,19 @@ public class WebServiceConfig extends WsConfigurerAdapter {
         wsdl11Definition.setTargetNamespace("http://www.atos.net/xml/pizzeria");
         wsdl11Definition.setSchema(schema);
         return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema pizzaSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("pizza.xsd"));
+    }
+
+    @Bean
+    public ServletRegistrationBean messegeServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/service/*");
     }
 
 }
